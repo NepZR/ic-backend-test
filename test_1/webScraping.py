34980f3cba4d525c226c3ecc5c5cc0af38b325d6
Lucas Rodrigues <lucas.darlindo@gmail.com>
2022-05-24 22:23:44 -0300

    Teste 01 - Web Scraping

---
+++ test_1/webScraping.py
@@ -0,0 +1,36 @@
+import requests
+import zipfile
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+TARGET_URL = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
+
+crawledPage = BeautifulSoup(
+    requests.get(TARGET_URL).text,
+    features='lxml'
+)
+
+html_targetList = crawledPage.find_all('a', attrs={
+    'target': '_self',
+    'class': 'internal-link'
+})
+
+attachmentList = [
+    item['href'] for item in html_targetList
+    if not (item.text.find('Anexo') == -1)
+]
+
+file_names = []
+for attachment in attachmentList:
+    urllib.request.urlretrieve(
+        attachment, attachment[attachment.index('Anexo'):]
+    )
+
+    file_names.append(
+        attachment[attachment.index('Anexo'):]
+    )
+
+with zipfile.ZipFile('attachments.zip', 'w') as attZip:
+    for file_name in file_names:
+        attZip.write(file_name)
